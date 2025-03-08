a2757c9a3a8733939566a1731942fb3649df2ec2
Sunny✨ <Sundaypius2000@gmail.com>
2025-03-08 23:33:18 +0100

    Update the constants file with new and updated folders and files paths

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -4,20 +4,30 @@ from pathlib import Path
 # Root directory of the project
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Data directory
-# DATA_ROOT_DIR = Path(__file__).resolve().parent.parent.parent
-
-# Data paths
+# Data directories
 RAW_DATA_DIR = os.path.join(ROOT_DIR, "data", "raw")
 PROCESSED_DATA_DIR = os.path.join(ROOT_DIR, "data", "processed")
+
+# Processed data subdirectories
+INGESTED_DATA_DIR = os.path.join(
+    PROCESSED_DATA_DIR, "ingested"
+)  # Data from ingestion stage
+VALIDATED_DATA_DIR = os.path.join(
+    PROCESSED_DATA_DIR, "validated"
+)  # Data from validation stage
+TRANSFORMED_DATA_DIR = os.path.join(
+    PROCESSED_DATA_DIR, "transformed"
+)  # Data from transformation stage
+
+# Schema and model configuration paths
 SCHEMA_FILE_PATH = os.path.join(ROOT_DIR, "src", "constants", "schema.yaml")
+MODEL_CONFIG_FILE = os.path.join(ROOT_DIR, "src", "constants", "model_config.yaml")
 
 # Database configuration
 DATABASE_NAME = "phishing_website_detection_analysis_db"
 COLLECTION_NAME = "phishing_data"
 
 # Model configuration
-MODEL_CONFIG_FILE = os.path.join(ROOT_DIR, "src", "constants", "model_config.yaml")
 BASE_ACCURACY = 0.85  # Minimum accuracy threshold for model acceptance
 
 # Logging configuration

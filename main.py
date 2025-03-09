bc8640834c0340f4e0d142b1d0e30ba7bd43aef7
Sunny✨ <Sundaypius2000@gmail.com>
2025-03-09 22:28:48 +0100

    Main file updated to handle both data ingestion and validation

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
 
 from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataIngestionConfig
+from src.components.data_validation import DataValidation
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from src.utils.exception import PhishingDetectionException
 from src.utils.logging import logger
 
 
 def main():
     try:
+
+        # DATA INGESTION
+
         logger.info("Starting the data ingestion process.")
 
         # Initialize DataIngestionConfig
@@ -24,6 +28,43 @@ def main():
         logger.info(f"Test file path: {data_ingestion_artifact.test_file_path}")
 
         print("Data ingestion process completed successfully!")
+
+        # *****************************************************************************************
+
+        # DATA VALIDATION
+
+        logger.info("Starting the data validation process.")
+
+        # Initialize DataValidationConfig
+        data_validation_config = DataValidationConfig()
+
+        # Initialize DataValidation
+        data_validation = DataValidation(
+            data_ingestion_artifact, data_validation_config
+        )
+
+        # Run the data validation process
+        data_validation_artifact = data_validation.initiate_data_validation()
+
+        logger.info("Data validation process completed successfully.")
+        logger.info(f"Validation status: {data_validation_artifact.validation_status}")
+        logger.info(
+            f"Valid train file path: {data_validation_artifact.valid_train_file_path}"
+        )
+        logger.info(
+            f"Valid test file path: {data_validation_artifact.valid_test_file_path}"
+        )
+        logger.info(
+            f"Invalid train file path: {data_validation_artifact.invalid_train_file_path}"
+        )
+        logger.info(
+            f"Invalid test file path: {data_validation_artifact.invalid_test_file_path}"
+        )
+        logger.info(
+            f"Drift report file path: {data_validation_artifact.drift_report_file_path}"
+        )
+
+        print("Data validation process completed successfully!")
 
     except Exception as e:
         logger.error(f"Error during data ingestion: {e}")

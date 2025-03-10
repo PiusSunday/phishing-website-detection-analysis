249c516e1443bb8de6896ee74d22a9597e5204a7
Sunny✨ <Sundaypius2000@gmail.com>
2025-03-11 00:20:37 +0100

    Updates made to the various config entities

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -5,18 +5,32 @@ import numpy as np
 
 from ..constants import (
     ARTIFACTS_DIR,
-    BASE_ACCURACY,
     COLLECTION_NAME,
     DATABASE_NAME,
+    DRIFT_REPORT_FILE_NAME,
+    FEATURE_STORE_FILE_NAME,
     INGESTED_DATA_DIR,
-    MODEL_CONFIG_FILE,
+    INVALID_TEST_FILE_NAME,
+    INVALID_TRAIN_FILE_NAME,
+    MODEL_TRAINER_DIR_NAME,
+    MODEL_TRAINER_EXPECTED_SCORE,
+    MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD,
+    MODEL_TRAINER_TRAINED_MODEL_DIR,
+    MODEL_TRAINER_TRAINED_MODEL_NAME,
     RAW_DATA_DIR,
     SCHEMA_FILE_PATH,
     TARGET_COLUMN,
+    TEST_FILE_NAME,
+    TRAIN_FILE_NAME,
     TRANSFORMED_DATA_DIR,
     TRANSFORMED_DATA_OBJECTS_DIR,
+    TRANSFORMED_OBJECT_FILE_NAME,
+    TRANSFORMED_TEST_FILE_NAME,
+    TRANSFORMED_TRAIN_FILE_NAME,
     VALIDATED_DATA_DIR,
     VALIDATED_DATA_OBJECTS_DIR,
+    VALID_TEST_FILE_NAME,
+    VALID_TRAIN_FILE_NAME,
 )
 
 
@@ -31,13 +45,13 @@ class DataIngestionConfig:
     database_name: str = DATABASE_NAME  # MongoDB database name
     collection_name: str = COLLECTION_NAME  # MongoDB collection name
     feature_store_file_path: str = os.path.join(
-        INGESTED_DATA_DIR, "feature_store.csv"
+        INGESTED_DATA_DIR, FEATURE_STORE_FILE_NAME
     )  # Path to feature store
     training_file_path: str = os.path.join(
-        INGESTED_DATA_DIR, "train.csv"
+        INGESTED_DATA_DIR, TRAIN_FILE_NAME
     )  # Path to training data
     testing_file_path: str = os.path.join(
-        INGESTED_DATA_DIR, "test.csv"
+        INGESTED_DATA_DIR, TEST_FILE_NAME
     )  # Path to testing data
     train_test_split_ratio: float = 0.2  # Train-test split ratio
 
@@ -56,19 +70,19 @@ class DataValidationConfig:
         VALIDATED_DATA_OBJECTS_DIR  # Directory to store validation objects
     )
     valid_train_file_path: str = os.path.join(
-        VALIDATED_DATA_DIR, "train.csv"
+        VALIDATED_DATA_DIR, VALID_TRAIN_FILE_NAME
     )  # Path to valid training data
     valid_test_file_path: str = os.path.join(
-        VALIDATED_DATA_DIR, "test.csv"
+        VALIDATED_DATA_DIR, VALID_TEST_FILE_NAME
     )  # Path to valid testing data
     invalid_train_file_path: str = os.path.join(
-        VALIDATED_DATA_DIR, "invalid_train.csv"
+        VALIDATED_DATA_DIR, INVALID_TRAIN_FILE_NAME
     )  # Path to invalid training data
     invalid_test_file_path: str = os.path.join(
-        VALIDATED_DATA_DIR, "invalid_test.csv"
+        VALIDATED_DATA_DIR, INVALID_TEST_FILE_NAME
     )  # Path to invalid testing data
     drift_report_file_path: str = os.path.join(
-        VALIDATED_DATA_OBJECTS_DIR, "drift_report.yaml"
+        VALIDATED_DATA_OBJECTS_DIR, DRIFT_REPORT_FILE_NAME
     )  # Path to drift report
 
 
@@ -93,13 +107,13 @@ class DataTransformationConfig:
         TRANSFORMED_DATA_OBJECTS_DIR  # Directory to store transformation objects
     )
     transformed_train_file_path: str = os.path.join(
-        TRANSFORMED_DATA_DIR, "train.npy"
+        TRANSFORMED_DATA_DIR, TRANSFORMED_TRAIN_FILE_NAME
     )  # Path to transformed training data
     transformed_test_file_path: str = os.path.join(
-        TRANSFORMED_DATA_DIR, "test.npy"
+        TRANSFORMED_DATA_DIR, TRANSFORMED_TEST_FILE_NAME
     )  # Path to transformed testing data
     transformed_object_file_path: str = os.path.join(
-        TRANSFORMED_DATA_OBJECTS_DIR, "preprocessor.pkl"
+        TRANSFORMED_DATA_OBJECTS_DIR, TRANSFORMED_OBJECT_FILE_NAME
     )  # Path to the transformation object
 
 
@@ -109,10 +123,18 @@ class ModelTrainingConfig:
     Configuration for model training.
     """
 
+    model_trainer_dir: str = os.path.join(
+        ARTIFACTS_DIR, MODEL_TRAINER_DIR_NAME
+    )  # Directory to store model training artifacts
     trained_model_dir: str = os.path.join(
-        ARTIFACTS_DIR, "trained_models"
-    )  # Directory to save trained models
-    base_accuracy: float = BASE_ACCURACY  # Minimum accuracy threshold
-    model_config_file: str = (
-        MODEL_CONFIG_FILE  # Path to model hyperparameters (e.g., JSON or YAML)
+        model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR
+    )  # Directory to store trained models
+    trained_model_file_path: str = os.path.join(
+        trained_model_dir, MODEL_TRAINER_TRAINED_MODEL_NAME
+    )  # Path to the trained model file
+    expected_accuracy: float = (
+        MODEL_TRAINER_EXPECTED_SCORE  # Minimum-expected accuracy for the model
+    )
+    overfitting_underfitting_threshold: float = (
+        MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD  # Threshold for overfitting/underfitting
     )
